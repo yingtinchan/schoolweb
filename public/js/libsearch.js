ac6adc01f420d73d1df6d74575f37c48627fd811
Oliverkywong <tom23400@gmail.com>
2023-10-21 16:56:36 +0800

    library

---
+++ public/js/libsearch.js
@@ -0,0 +1,9 @@
+$('.typebutton').click(function(){
+    $('.typebutton').toggleClass('active');
+    $('.libsearch').toggleClass('hide');
+    $('.libsearch').show();
+    $('.libsearch.hide').hide();
+})
+
+$('.libsearch.hide').hide();
+
