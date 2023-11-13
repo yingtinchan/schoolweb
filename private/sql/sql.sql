b1991a6a55db623fedd72a7d46ac621074858f2f
KayP <kaypoon1221@gmail.com>
2023-11-13 20:41:56 +0800

    update sql

--- private/sql/sql.sql
+++ private/sql/sql.sql
@@ -3,3 +3,5 @@ CREATE TABLE student (student_id VARCHAR(255),name VARCHAR(255),password VARCHAR
 CREATE TABLE teacher (teacher_id VARCHAR(255),name VARCHAR(255),password VARCHAR(255),email VARCHAR(255),created_at TIMESTAMP,updated_at TIMESTAMP,token VARCHAR(1000),PRIMARY KEY (teacher_id));
 
 CREATE TABLE admin (admin_id VARCHAR(255),name VARCHAR(255),password VARCHAR(255),email VARCHAR(255),created_at TIMESTAMP,updated_at TIMESTAMP,token VARCHAR(1000),PRIMARY KEY (admin_id));
+
+CREATE TABLE major (major_id VARCHAR(255),name VARCHAR(255),PRIMARY KEY (major_id));
